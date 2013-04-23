79c375d0454122f0c4390cee9db7d6e4503ecb81
l3thal <l3thal@smashthestack.org>
2013-04-23 17:48:22 -0400

    create_table video instead of change_table

--- lib/generators/s3_multipart/templates/add_uploader_column_to_model.rb
+++ lib/generators/s3_multipart/templates/add_uploader_column_to_model.rb
@@ -1,6 +1,6 @@
 class AddUploaderTo<%= model_constant %> < ActiveRecord::Migration
   def change
-    change_table :<%= model %> do |t|
+    create_table :<%= model %> do |t|
       t.string :uploader
     end
   end
